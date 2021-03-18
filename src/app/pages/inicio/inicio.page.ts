c2f8a24e0907d5a44dfc21b8494d519b463a45c8
Angel Daniel Aviles cruz <angeldaniel.avilescruz66@gmail.com>
2021-03-17 23:36:03 -0600

    ion-button

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -28,6 +28,11 @@ export class InicioPage implements OnInit {
       icon: 'beaker',
       name: 'Avatar',
       redirectTo: '/avatar'
+    },
+    {
+      icon: 'radio-button-on',
+      name: 'Buttons',
+      redirectTo: '/button'
     }
   ]
 
