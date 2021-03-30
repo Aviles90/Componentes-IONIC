4ff2f3b59da1798d7e42a6e887fdc1e92b9372b7
Angel Daniel Aviles cruz <angeldaniel.avilescruz66@gmail.com>
2021-03-29 23:05:39 -0600

    ion-segment

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -17,6 +17,10 @@ export class DataService {
     return this.http.get<Componente[]>('/assets/data/menu-opts.json');
   }
 
+  getHeroes(){
+    return this.http.get<Componente[]>('/assets/data/superheroes.json');
+  }
+
   getAlbumes(){
     return this.http.get<any[]>('https://jsonplaceholder.typicode.com/albums');
   }
